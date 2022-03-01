2f2b1f0c350a4290b072498b0cd98212345cb322
GiovanniCalia <johnca@outlook.it>
2022-03-01 15:55:01 +0100

    Completamento palindroma, ma senza funzione

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
 /* PALINDROMA */
 
-let nome = prompt("Inserisci una parola");
+let nome = prompt("Ciao! Inserisci una parola ed io ti dirò se è palindroma o no!");
+let risultato = document.getElementById("stampa");
+let parola = nome.split("");
+let parola2 = parola.join();
+let parolaInversa = parola.reverse();
+let parolaInvertita = parolaInversa.join();
+console.log(parola2);
+console.log(parolaInvertita)
+
+
+if (parola2 == parolaInvertita){
+    risultato.innerHTML = (`Ottimo! "${nome}" è una parola palindroma!`)
+} else {
+    risultato.innerHTML = (`No, "${nome}" non è una parola palindroma.`)
+}
+
+
+
+
 
 
