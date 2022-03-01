a44136a8614251c26affbf8754eecba38c56825c
GiovanniCalia <johnca@outlook.it>
2022-03-01 18:39:44 +0100

    Continuazione esercizio pari dispari

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 /* PALINDROMA */
 
+/*
 let nome = prompt("Ciao! Inserisci una parola ed io ti dirò se è palindroma o no!");
 let risultato = document.getElementById("stampa");
 let parolaSplit = nome.split("");
@@ -13,13 +14,27 @@ if (parola.toLowerCase == parolaInvertita.toLowerCase){
 } else {
     risultato.innerHTML = (`No, "${nome}" non è una parola palindroma.`)
 }
+*/
 
 /* PARI E DISPARI */
 
-const pariDispari = prompt("Scegli pari o dispari")
-    switch (pariDispari){
-        case "pari":     
-    }
+function isEven(testNumber) {
+    alert(testNumber);
+}
+
+
+let pariDispari = parseInt(prompt("Scelga pari o dispari, in un numero compreso da 1 a 5"));
+
+if ( pariDispari < 1 || pariDispari > 5){
+    isEven("Spiacente, il numero non è compreso tra 1 e 5");
+} else if (pariDispari % 2 == 0){
+    isEven("Il numero è pari");
+} else if (pariDispari % 2 == 1) {
+    isEven("Il numero è dispari");
+} else{
+    isEven("Inserisca un numero");
+}
+
 
 
 
