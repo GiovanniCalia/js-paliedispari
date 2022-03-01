bb07f360d3695625602475c70af6813f7c456b59
GiovanniCalia <johnca@outlook.it>
2022-03-01 16:11:18 +0100

    Inizio esercizio "pari e dispari"

--- js/script.js
+++ js/script.js
@@ -2,19 +2,29 @@
 
 let nome = prompt("Ciao! Inserisci una parola ed io ti dirò se è palindroma o no!");
 let risultato = document.getElementById("stampa");
-let parola = nome.split("");
-let parola2 = parola.join();
-let parolaInversa = parola.reverse();
+let parolaSplit = nome.split("");
+let parola = parolaSplit.join();
+let parolaInversa = parolaSplit.reverse();
 let parolaInvertita = parolaInversa.join();
-console.log(parola2);
-console.log(parolaInvertita)
 
 
-if (parola2 == parolaInvertita){
+if (parola.toLowerCase == parolaInvertita.toLowerCase){
     risultato.innerHTML = (`Ottimo! "${nome}" è una parola palindroma!`)
 } else {
     risultato.innerHTML = (`No, "${nome}" non è una parola palindroma.`)
 }
+
+/* PARI E DISPARI */
+
+const pariDispari = prompt("Scegli pari o dispari")
+    switch (pariDispari){
+        case "pari":     
+    }
+
+
+
+
+
 
 
 
